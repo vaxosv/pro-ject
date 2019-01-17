aeda7fae0c3a2bd3bd7a2f759e31f3da0e4c6874
vaxosv <vaxo.sv@gmail.com>
2019-01-17 19:16:16 +0400

    users

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from "../data.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"]
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  value: boolean = false;
+  users: any;
+  constructor(private data: DataService) {}
 
   ngOnInit() {
+    this.data.getusers().subscribe(data => {
+      this.users = data;
+      console.log(data);
+      
+    })
   }
 
+  firstClick() {
+    // this.data.data()
+    // this.aa = this.data.getusers()
+    // this.value = !this.value;
+    
+  }
 }
